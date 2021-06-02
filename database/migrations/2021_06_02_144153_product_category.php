657b4448d3178d84fb28cbb833be72b330f34666
Maika Kanaka <maika-kanaka@protonmail.com>
2021-06-02 22:05:44 +0700

    buat menu produk & kategori produk dengan BREAD <3

---
+++ database/migrations/2021_06_02_144153_product_category.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('nama_kategori', 120)->nullable();
+            $table->string('deskripsi', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_categories');
+    }
+}
---
+++ database/migrations/2021_06_02_144801_product.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Product extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('nama_produk', 255)->nullable();
+            $table->string('deskripsi', 255)->nullable();
+            $table->string('gambar', 255)->nullable();
+            $table->integer('kategori')->nullable();
+            $table->integer('stok')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
